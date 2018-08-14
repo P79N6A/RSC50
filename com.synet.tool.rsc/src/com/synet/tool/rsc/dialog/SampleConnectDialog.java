241175b80f870e2164efff840ccf1c1eb7081be8
DevLiMengLong <15800714637@163.com>
2018-08-14 20:08:48 +0800

    Update:一次拓扑模型界面完成；保护信息模型界面50%

--- com.synet.tool.rsc/src/com/synet/tool/rsc/dialog/SampleConnectDialog.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/dialog/SampleConnectDialog.java
@@ -1,12 +1,31 @@
 package com.synet.tool.rsc.dialog;
 
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
 
-import com.shrcn.found.ui.app.WrappedTitleAreaDialog;
+import com.shrcn.found.ui.app.WrappedDialog;
+import com.shrcn.found.ui.util.SwtUtil;
+import com.synet.tool.rsc.ui.TableFactory;
+import com.synet.tool.rsc.ui.table.DevKTable;
 
-public class SampleConnectDialog extends WrappedTitleAreaDialog {
+public class SampleConnectDialog extends WrappedDialog {
 
 	private String curEntryName;
+	private DevKTable tableProtctSample;
+	private Button btnMove;
+	private Button btnSearch;
+	private DevKTable tableSample;
+	private String[] comboItems;
 	
 	public SampleConnectDialog(Shell parentShell) {
 		super(parentShell);
@@ -15,6 +34,77 @@ public class SampleConnectDialog extends WrappedTitleAreaDialog {
 	public SampleConnectDialog(Shell defaultShell, String curEntryName) {
 		super(defaultShell);
 		this.curEntryName = curEntryName;
+		comboItems = new String[]{"智能终端1"};
+	}
+	
+	@Override
+	protected Control createDialogArea(Composite parent) {
+		GridData gridData = new GridData(GridData.FILL_BOTH);
+		Composite composite = SwtUtil.createComposite(parent, gridData, 1);
+		composite.setLayout(SwtUtil.getGridLayout(2));
+		//左侧
+		Composite comLeft = SwtUtil.createComposite(composite, gridData, 1);
+		comLeft.setLayout(SwtUtil.getGridLayout(2));
+		GridData gdlb_2 = new GridData(200,25);
+		gdlb_2.horizontalSpan = 2;
+		String switchLbName = curEntryName + "互感器保护采样值：";
+		SwtUtil.createLabel(comLeft, switchLbName, gdlb_2);
+		
+		tableProtctSample = TableFactory.getProtAnalogTable(comLeft);
+		tableProtctSample.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		btnMove = SwtUtil.createButton(comLeft, new GridData(40, SWT.DEFAULT), SWT.BUTTON1, "<-");
+		//右侧
+		Composite comRight = SwtUtil.createComposite(composite, gridData, 1);
+		comRight.setLayout(SwtUtil.getGridLayout(3));
+		
+		Combo combo = SwtUtil.createCombo(comRight, SwtUtil.bt_hd);
+		combo.setItems(comboItems);
+		combo.select(0);
+		
+		Text text = SwtUtil.createText(comRight, SwtUtil.bt_hd);
+		text.setMessage("描述");
+		
+		btnSearch = SwtUtil.createButton(comRight, new GridData(50, 25), SWT.BUTTON1, "查询");
+		GridData gdSpan_3 = new GridData(GridData.FILL_BOTH);
+		gdSpan_3.horizontalSpan = 3;
+		tableSample = TableFactory.getAnalogTable(comRight);
+		tableSample.getTable().setLayoutData(gdSpan_3);
+		addListeners();
+		return composite;
+	}
+	
+	
+	@Override
+	protected void configureShell(Shell newShell) {
+		super.configureShell(newShell);
+		newShell.setText("互感器通道关联");
+	}
+	
+	private void addListeners() {
+		btnMove.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				super.widgetSelected(e);
+			}
+		});
+		btnSearch.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				super.widgetSelected(e);
+			}
+		});
+		
+	}
+
+	@Override
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, "OK", true);
+		createButton(parent, IDialogConstants.CANCEL_ID, "取消", true);
+	}
+	
+	@Override
+	protected Point getInitialSize() {
+		return new Point(800, 550);
 	}
 
 }
--- com.synet.tool.rsc/src/com/synet/tool/rsc/editor/ProtectIEDlEditor.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/editor/ProtectIEDlEditor.java
@@ -7,11 +7,19 @@ package com.synet.tool.rsc.editor;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 
+import com.shrcn.found.ui.editor.ConfigEditorInput;
 import com.shrcn.found.ui.editor.IEditorInput;
 import com.shrcn.found.ui.util.SwtUtil;
+import com.synet.tool.rsc.ui.TableFactory;
+import com.synet.tool.rsc.ui.table.DevKTable;
 
 /**
  * 保护信息模型->装置树菜单编辑器。
@@ -20,36 +28,316 @@ import com.shrcn.found.ui.util.SwtUtil;
  */
 public class ProtectIEDlEditor extends BaseConfigEditor {
 	
+	private Button btnTempCamp;
+	private Button btnTempQuote;
+	private Button btnTempSave;
+	private String editorName;
+	private GridData gridData;
+	private DevKTable tableBoardPort;
+	private DevKTable tableProtectValue;
+	private DevKTable tableProtParam;
+	private DevKTable tableProtectPlate;
+	private DevKTable tableProtectAction;
+	private DevKTable tableProtectMeaQuantity;
+	private DevKTable tableDeviceWarning;
+	private Button btnConnectDeviceWarning;
+	private DevKTable tableRunState;
+	private Button btnConnectRunState;
+	private DevKTable tableLogicalLink;
+	private Button btnConnectLogicalLink;
+	private DevKTable tableVirtualTerminalOut;
+	private DevKTable tableVirtualTerminalIn;
+	private DevKTable tableAnalogChn;
+	private DevKTable tableCriteriaChn;
+
 	public ProtectIEDlEditor(Composite container, IEditorInput input) {
 		super(container, input);
+	}
+	
+	@Override
+	public void init() {
+		ConfigEditorInput input = (ConfigEditorInput) getInput();
+		editorName = input.getEditorName();
+		gridData = new GridData(GridData.FILL_BOTH);
+		super.init();
 	}
 
 	@Override
 	public void buildUI(Composite container) {
 		super.buildUI(container);
-		CTabFolder tab = SwtUtil.createTabFolder(editArea, SWT.TOP | SWT.BORDER);
-		tab.setLayoutData(new GridData(GridData.FILL_BOTH));
-		
-//		// 基本信息
-//		Composite baseCmp = SwtUtil.createComposite(tab, new GridData(GridData.FILL_VERTICAL), 1);
-//		SwtUtil.addTabItem(tab, "基本信息", baseCmp);
-//		baseInfoTbl = TableFactory.getBaseInfoTable(baseCmp);
-//		baseInfoTbl.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
-//		
-//		// 板卡信息
-//		Composite cardCmp = SwtUtil.createComposite(tab, new GridData(GridData.FILL_BOTH), 1);
-//		SwtUtil.addTabItem(tab, "板卡信息", cardCmp);
-//		cardInfoTbl = TableFactory.getCardInfoTable(cardCmp);
-//		cardInfoTbl.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
-//		
-//		tab.setSelection(0);
+		Composite comp = SwtUtil.createComposite(container, gridData, 1);
+		comp.setLayout(SwtUtil.getGridLayout(4));
+		SwtUtil.createLabel(comp, "", new GridData(780, SWT.DEFAULT));
+		btnTempCamp = SwtUtil.createButton(comp, SwtUtil.bt_gd, SWT.BUTTON1, "对比模版");
+		btnTempQuote = SwtUtil.createButton(comp, SwtUtil.bt_gd, SWT.BUTTON1, "引用模版");
+		btnTempSave = SwtUtil.createButton(comp, SwtUtil.bt_gd, SWT.BUTTON1, "保存模版");
+		createCompByEntryName(comp);
 	}
 	
+	private void createCompByEntryName(Composite comp) {
+		GridData gdSpan_4 = new GridData(GridData.FILL_BOTH);
+		gdSpan_4.horizontalSpan = 4;
+		switch (editorName) {
+		case "保护":
+			createProtectCmp(comp, gdSpan_4);
+			break;
+		case "合并单元":
+			createMergeUnitCmp(comp, gdSpan_4);
+			break;
+		case "智能终端":
+			createIedCmp(comp, gdSpan_4);
+			break;
+		default:
+			break;
+		}
+		
+	}
+
+	/**
+	 * 创建智能终端界面
+	 * @param comp
+	 * @param gdSpan_4
+	 */
+	private void createIedCmp(Composite comp, GridData gdSpan_4) {
+		String[] tabNames = new String[]{"板卡端口", "装置告警", "运行工况", "虚端子压板", "逻辑链路"};
+		Control[] controls = getControls(comp, gdSpan_4, tabNames);
+		//板卡端口
+		createBoardPortCmp((Composite) controls[0]);
+		//装置告警
+		createDeviceWarningCmp((Composite) controls[1]);
+		//运行工况
+		createRunStateCmp((Composite) controls[2]);
+		//虚端子压板
+		createVirtualTerminalPlateCmp((Composite) controls[3]);
+		//逻辑链路
+		createLogicalLinkCmp((Composite) controls[4]);
+	}
+
+	/**
+	 * 创建合并单元界面
+	 * @param comp
+	 * @param gdSpan_4
+	 */
+	private void createMergeUnitCmp(Composite comp, GridData gdSpan_4) {
+		String[] tabNames = new String[]{"板卡端口", "装置告警", "运行工况"};
+		Control[] controls = getControls(comp, gdSpan_4, tabNames);
+		//板卡端口
+		createBoardPortCmp((Composite) controls[0]);
+		//装置告警
+		createDeviceWarningCmp((Composite) controls[1]);
+		//运行工况
+		createRunStateCmp((Composite) controls[2]);
+	}
+
+	/**
+	 * 创建保护界面
+	 * @param comp
+	 * @param gdSpan_4
+	 */
+	private void createProtectCmp(Composite comp, GridData gdSpan_4) {
+		String[] tabNames = new String[]{"板卡端口", "保护信息", "装置告警", "运行工况", "虚端子压板", "逻辑链路", "保护录波"};
+		Control[] controls = getControls(comp, gdSpan_4, tabNames);
+		//板卡端口
+		createBoardPortCmp((Composite) controls[0]);
+		//保护信息
+		createProtMsgCmp((Composite) controls[1]);
+		//装置告警
+		createDeviceWarningCmp((Composite) controls[2]);
+		//运行工况
+		createRunStateCmp((Composite) controls[3]);
+		//虚端子压板
+		createVirtualTerminalPlateCmp((Composite) controls[4]);
+		//逻辑链路
+		createLogicalLinkCmp((Composite) controls[5]);
+		//保护录波
+		createProtWaveCmp((Composite) controls[6]);
+	}
+
+	/**
+	 * 创建TbFolder,返回子控件集合
+	 * @param comp
+	 * @param gdSpan_4
+	 * @param tabNames
+	 * @return
+	 */
+	private Control[] getControls(Composite comp, GridData gdSpan_4,
+			String[] tabNames) {
+		CTabFolder tabFolder = SwtUtil.createTab(comp, gdSpan_4, tabNames);
+		tabFolder.setSelection(0);
+		Control[] controls = tabFolder.getChildren();
+		return controls;
+	}
+
 	protected void addListeners() {
+		SelectionListener selectionListener = new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				
+				super.widgetSelected(e);
+			}
+		};
+		btnTempCamp.addSelectionListener(selectionListener);
+		btnTempQuote.addSelectionListener(selectionListener);
+		btnTempSave.addSelectionListener(selectionListener);
+		btnConnectDeviceWarning.addSelectionListener(selectionListener);
+		btnConnectRunState.addSelectionListener(selectionListener);
+		switch (editorName) {
+		case "保护":
+			btnConnectLogicalLink.addSelectionListener(selectionListener);
+			break;
+		case "合并单元":
+			
+			break;
+		case "智能终端":
+			btnConnectLogicalLink.addSelectionListener(selectionListener);
+			break;
+		default:
+			break;
+		}
 	}
 
 	@Override
 	public void initData() {
 		super.initData();
 	}
+	
+	/**
+	 * 创建板卡端口界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createBoardPortCmp(Composite com) {
+		Composite cmpBoardPort = SwtUtil.createComposite(com, gridData, 1);
+		cmpBoardPort.setLayout(SwtUtil.getGridLayout(1));
+		tableBoardPort = TableFactory.getAnalogTable(cmpBoardPort);
+		tableBoardPort.getTable().setLayoutData(gridData);
+		return cmpBoardPort;
+	}
+	
+	/**
+	 * 创建保护信息界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createProtMsgCmp(Composite com) {
+		Composite cmpProtMsg = SwtUtil.createComposite(com, gridData, 1);
+		cmpProtMsg.setLayout(SwtUtil.getGridLayout(1));
+		String[] tabNames = new String[]{"保护定值", "保护参数", "保护压板", "保护动作", "保护测量量"};
+		CTabFolder tabFolder = SwtUtil.createTab(cmpProtMsg, gridData, tabNames );
+		tabFolder.setSelection(0);
+		Control[] controls = tabFolder.getChildren();
+		//保护定值
+		Composite cmpProtValue = SwtUtil.createComposite((Composite) controls[0], gridData, 1);
+		tableProtectValue = TableFactory.getAnalogTable(cmpProtValue);
+		tableProtectValue.getTable().setLayoutData(gridData);
+		//保护参数
+		Composite cmpProtParam = SwtUtil.createComposite((Composite) controls[1], gridData, 1);
+		tableProtParam = TableFactory.getAnalogTable(cmpProtParam);
+		tableProtParam.getTable().setLayoutData(gridData);
+		//保护压板
+		Composite cmpProtPlate = SwtUtil.createComposite((Composite) controls[2], gridData, 1);
+		tableProtectPlate = TableFactory.getAnalogTable(cmpProtPlate);
+		tableProtectPlate.getTable().setLayoutData(gridData);
+		//保护动作
+		Composite cmpProtAction = SwtUtil.createComposite((Composite) controls[3], gridData, 1);
+		tableProtectAction = TableFactory.getAnalogTable(cmpProtAction);
+		tableProtectAction.getTable().setLayoutData(gridData);
+		//保护测量量
+		Composite cmpProtMeaQuantity = SwtUtil.createComposite((Composite) controls[4], gridData, 1);
+		tableProtectMeaQuantity = TableFactory.getAnalogTable(cmpProtMeaQuantity);
+		tableProtectMeaQuantity.getTable().setLayoutData(gridData);
+		
+		return cmpProtMsg;
+	}
+	
+	/**
+	 * 创建装置告警界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createDeviceWarningCmp(Composite com) {
+		Composite cmpDeviceWarning = SwtUtil.createComposite(com, gridData, 1);
+		cmpDeviceWarning.setLayout(SwtUtil.getGridLayout(1));
+		btnConnectDeviceWarning = SwtUtil.createButton(cmpDeviceWarning, SwtUtil.bt_gd, SWT.BUTTON1, "关联");
+		tableDeviceWarning = TableFactory.getAnalogTable(cmpDeviceWarning);
+		tableDeviceWarning.getTable().setLayoutData(gridData);
+		return cmpDeviceWarning;
+	}
+	
+	/**
+	 * 创建运行工况界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createRunStateCmp(Composite com) {
+		Composite cmpRunState = SwtUtil.createComposite(com, gridData, 1);
+		cmpRunState.setLayout(SwtUtil.getGridLayout(1));
+		btnConnectRunState = SwtUtil.createButton(cmpRunState, SwtUtil.bt_gd, SWT.BUTTON1, "关联");
+		tableRunState = TableFactory.getAnalogTable(cmpRunState);
+		tableRunState.getTable().setLayoutData(gridData);
+		return cmpRunState;
+	}
+	
+	/**
+	 * 创建虚端子压板界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createVirtualTerminalPlateCmp(Composite com) {
+		Composite cmpVirtualTerminalPlate = SwtUtil.createComposite(com, gridData, 1);
+		cmpVirtualTerminalPlate.setLayout(SwtUtil.getGridLayout(1));
+		String[] tabNames = new String[]{"开出虚端子", "开入虚端子"};
+		CTabFolder tabFolder = SwtUtil.createTab(cmpVirtualTerminalPlate, gridData, tabNames );
+		tabFolder.setSelection(0);
+		Control[] controls = tabFolder.getChildren();
+		//开出虚端子
+		Composite cmpVirtualTerminalOut = SwtUtil.createComposite((Composite) controls[0], gridData, 1);
+		tableVirtualTerminalOut = TableFactory.getAnalogTable(cmpVirtualTerminalOut);
+		tableVirtualTerminalOut.getTable().setLayoutData(gridData);
+		//开入虚端子
+		Composite cmpVirtualTerminalIn = SwtUtil.createComposite((Composite) controls[1], gridData, 1);
+		tableVirtualTerminalIn = TableFactory.getAnalogTable(cmpVirtualTerminalIn);
+		tableVirtualTerminalIn.getTable().setLayoutData(gridData);
+		
+		return cmpVirtualTerminalPlate;
+	}
+	
+	/**
+	 * 创建逻辑链路界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createLogicalLinkCmp(Composite com) {
+		Composite cmpLogicalLink = SwtUtil.createComposite(com, gridData, 1);
+		cmpLogicalLink.setLayout(SwtUtil.getGridLayout(1));
+		btnConnectLogicalLink = SwtUtil.createButton(cmpLogicalLink, SwtUtil.bt_gd, SWT.BUTTON1, "关联");
+		tableLogicalLink = TableFactory.getAnalogTable(cmpLogicalLink);
+		tableLogicalLink.getTable().setLayoutData(gridData);
+		return cmpLogicalLink;
+	}
+	
+	/**
+	 * 创建保护录波界面
+	 * @param com
+	 * @return
+	 */
+	private Composite createProtWaveCmp(Composite com) {
+		Composite cmpProtWave = SwtUtil.createComposite(com, gridData, 1);
+		cmpProtWave.setLayout(SwtUtil.getGridLayout(1));
+		String[] tabNames = new String[]{"模拟量通道", "状态量通道"};
+		CTabFolder tabFolder = SwtUtil.createTab(cmpProtWave, gridData, tabNames );
+		tabFolder.setSelection(0);
+		Control[] controls = tabFolder.getChildren();
+		//模拟量通道
+		Composite cmpAnalogChn = SwtUtil.createComposite((Composite) controls[0], gridData, 1);
+		tableAnalogChn = TableFactory.getAnalogTable(cmpAnalogChn);
+		tableAnalogChn.getTable().setLayoutData(gridData);
+		//状态量通道
+		Composite cmpCriteriaChn = SwtUtil.createComposite((Composite) controls[1], gridData, 1);
+		tableCriteriaChn = TableFactory.getAnalogTable(cmpCriteriaChn);
+		tableCriteriaChn.getTable().setLayoutData(gridData);
+		
+		return cmpProtWave;
+	}
+	
 }
