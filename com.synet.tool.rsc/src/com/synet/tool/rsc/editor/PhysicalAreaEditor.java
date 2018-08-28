7e4c68543e03bb14064d6e57cad1d6f377d2f3ce
github-minwang <minwang.gml@gmail.com>
2018-08-29 00:58:19 +0800

    Update：添加手动数据添加、删除功能

--- com.synet.tool.rsc/src/com/synet/tool/rsc/editor/PhysicalAreaEditor.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/editor/PhysicalAreaEditor.java
@@ -9,7 +9,10 @@ import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CTabFolder;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 import com.shrcn.found.ui.editor.EditorConfigData;
@@ -23,6 +26,7 @@ import com.synet.tool.rsc.model.Tb1053PhysconnEntity;
 import com.synet.tool.rsc.service.PhyscialAreaService;
 import com.synet.tool.rsc.ui.TableFactory;
 import com.synet.tool.rsc.ui.table.DevKTable;
+import com.synet.tool.rsc.util.RscObjectUtils;
 
 /**
  * 物理信息模型->区域树菜单编辑器。
@@ -34,8 +38,14 @@ public class PhysicalAreaEditor extends BaseConfigEditor {
 	private DevKTable cubicleTable;
 	private DevKTable cableTable;
 	private DevKTable physConnTable;
+	private Button btnAddCubicle;
+	private Button btnAddCable;
+	private Button btnAddPhysconn;
+	private Button btnDelCubicle;
+	private Button btnDelCable;
+	private Button btnDelPhysconn;
 	private PhyscialAreaService areaService;
-	private Tb1049RegionEntity entity;
+	private Tb1049RegionEntity regionEntity;
 	
 	public PhysicalAreaEditor(Composite container, IEditorInput input) {
 		super(container, input);
@@ -45,7 +55,7 @@ public class PhysicalAreaEditor extends BaseConfigEditor {
 	public void init() {
 		areaService = new PhyscialAreaService();
 		EditorConfigData data = (EditorConfigData) getInput().getData();
-		entity = (Tb1049RegionEntity) data.getData();
+		regionEntity = (Tb1049RegionEntity) data.getData();
 		super.init();
 	}
 
@@ -54,33 +64,68 @@ public class PhysicalAreaEditor extends BaseConfigEditor {
 		super.buildUI(container);
 		tab = SwtUtil.createTabFolder(container, SWT.TOP | SWT.BORDER);
 		tab.setLayoutData(new GridData(GridData.FILL_BOTH));
-		Composite cubicleComp = SwtUtil.createComposite(tab, new GridData(), 1);
+		Composite cubicleComp = SwtUtil.createComposite(tab, new GridData(), 2);
 		SwtUtil.addTabItem(tab, RSCConstants.TAB_CUBICLE, cubicleComp);
-		Composite cableComp = SwtUtil.createComposite(tab, new GridData(), 1);
+		Composite cableComp = SwtUtil.createComposite(tab, new GridData(), 2);
 		SwtUtil.addTabItem(tab, RSCConstants.TAB_CABLE, cableComp);
-		Composite physConnComp = SwtUtil.createComposite(tab, new GridData(), 1);
+		Composite physConnComp = SwtUtil.createComposite(tab, new GridData(), 2);
 		SwtUtil.addTabItem(tab, RSCConstants.TAB_PHYSCONN, physConnComp);
 		tab.setSelection(0);
 		
+		GridData tableGridData = new GridData(GridData.FILL_BOTH);
+		tableGridData.horizontalSpan = 2;
+		
+		btnAddCubicle = SwtUtil.createButton(cubicleComp, SwtUtil.bt_gd, SWT.BUTTON1, "添加");
+		btnDelCubicle = SwtUtil.createButton(cubicleComp, SwtUtil.bt_gd, SWT.BUTTON1, "删除");		
 		cubicleTable = TableFactory.getCubicleTable(cubicleComp);
-		cubicleTable.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		cubicleTable.getTable().setLayoutData(tableGridData);
 		
+		btnAddCable = SwtUtil.createButton(cableComp, SwtUtil.bt_gd, SWT.BUTTON1, "添加");
+		btnDelCable = SwtUtil.createButton(cableComp, SwtUtil.bt_gd, SWT.BUTTON1, "删除");	
 		cableTable = TableFactory.getCableTable(cableComp);
-		cableTable.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		cableTable.getTable().setLayoutData(tableGridData);
 		
+		btnAddPhysconn = SwtUtil.createButton(physConnComp, SwtUtil.bt_gd, SWT.BUTTON1, "添加");
+		btnDelPhysconn = SwtUtil.createButton(physConnComp, SwtUtil.bt_gd, SWT.BUTTON1, "删除");	
 		physConnTable = TableFactory.getPhysconnTable(physConnComp);
-		physConnTable.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		physConnTable.getTable().setLayoutData(tableGridData);
 
 	}
 	
 	protected void addListeners() {
+		SelectionAdapter listener = new SelectionAdapter() {
+			
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Object obj = e.getSource();
+				if (obj == btnAddCubicle) {
+					addCubicle();
+				} else if (obj == btnDelCubicle) {
+					delCubicle();
+				} else if (obj == btnAddCable) {
+					addCable();
+				} else if (obj == btnDelCable) {
+					delCable();
+				} else if (obj == btnAddPhysconn) {
+					addPhysconn();
+				} else if (obj == btnDelPhysconn) {
+					delPhysconn();
+				}
+			}
+		};
 		
+		btnAddCubicle.addSelectionListener(listener);
+		btnAddCable.addSelectionListener(listener);
+		btnAddPhysconn.addSelectionListener(listener);
+		btnDelCubicle.addSelectionListener(listener);
+		btnDelCable.addSelectionListener(listener);
+		btnDelPhysconn.addSelectionListener(listener);
 	}
 
 	@Override
 	public void initData() {
 		List<Tb1050CubicleEntity> cubicleList = 
-				(List<Tb1050CubicleEntity>) areaService.getCubicleList(entity);
+				(List<Tb1050CubicleEntity>) areaService.getCubicleList(regionEntity);
 		if (cubicleList != null) {
 			cubicleTable.setInput(cubicleList);
 		}
@@ -96,5 +141,41 @@ public class PhysicalAreaEditor extends BaseConfigEditor {
 		
 		if (physconnList != null)
 			physConnTable.setInput(physconnList);
+	}
+	
+
+	private void delPhysconn() {
+		if (physConnTable.getSelection() != null) {
+			areaService.delete(physConnTable.getSelection());
+			physConnTable.removeSelected();
+		}
+	}
+
+	private void addPhysconn() {
+		physConnTable.addRow(RscObjectUtils.createPhysconn());
+	}
+
+	private void delCable() {
+		if (cableTable.getSelection() != null) {
+			areaService.delete(cableTable.getSelection());
+			cableTable.removeSelected();
+		}
+	}
+
+	private void addCable() {
+		cableTable.addRow(RscObjectUtils.createCable());
+	}
+
+	private void delCubicle() {
+		if (cubicleTable.getSelection() != null) {
+			areaService.delete(cubicleTable.getSelection());
+			cubicleTable.removeSelected();
+		}
+	}
+
+	private void addCubicle() {
+		Tb1050CubicleEntity cubicleEntity = RscObjectUtils.createCubicle();
+		cubicleEntity.setTb1049RegionByF1049Code(regionEntity);
+		cubicleTable.addRow(cubicleEntity);
 	}
 }
--- com.synet.tool.rsc/src/com/synet/tool/rsc/editor/PhysicalModelEditor.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/editor/PhysicalModelEditor.java
@@ -7,15 +7,24 @@ package com.synet.tool.rsc.editor;
 
 import java.util.List;
 
+import oracle.net.aso.s;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 
 import com.shrcn.found.ui.editor.EditorConfigData;
 import com.shrcn.found.ui.editor.IEditorInput;
 import com.shrcn.found.ui.util.SwtUtil;
 import com.shrcn.tool.found.das.impl.BeanDaoImpl;
+import com.synet.tool.rsc.dialog.AddRegionDialog;
 import com.synet.tool.rsc.model.Tb1049RegionEntity;
+import com.synet.tool.rsc.service.PhysicalModelService;
 import com.synet.tool.rsc.ui.TableFactory;
+import com.synet.tool.rsc.util.RscObjectUtils;
 
 /**
  * 物理信息模型树菜单编辑器。
@@ -24,26 +33,70 @@ import com.synet.tool.rsc.ui.TableFactory;
  */
 public class PhysicalModelEditor extends BaseConfigEditor {
 	
+	private Button btnAddRegion;
+	private Button btnDelRegion;
+	private PhysicalModelService service;
 	
 	public PhysicalModelEditor(Composite container, IEditorInput input) {
 		super(container, input);
+	}
+	
+	@Override
+	public void init() {
+		service = new PhysicalModelService();
+		super.init();
 	}
 
 	@Override
 	public void buildUI(Composite container) {
 		super.buildUI(container);
-		editArea.setLayout(SwtUtil.getGridLayout(1));
-		table =TableFactory.getAreaListTable(editArea);
-		table.getTable().setLayoutData(new GridData(GridData.FILL_BOTH));
+		container.setLayout(SwtUtil.getGridLayout(2));
+		btnAddRegion = SwtUtil.createButton(container, SwtUtil.bt_gd, SWT.BUTTON1, "添加");
+		btnDelRegion = SwtUtil.createButton(container, SwtUtil.bt_gd, SWT.BUTTON1, "删除");
 		
+		GridData tableGridData = new GridData(GridData.FILL_BOTH);
+		tableGridData.horizontalSpan = 2;
+		table =TableFactory.getAreaListTable(container);
+		table.getTable().setLayoutData(tableGridData);
 	}
 	
 	protected void addListeners() {
+		SelectionAdapter listener = new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				Object obj = e.getSource();
+				if (obj == btnAddRegion) {
+					addRegion();
+				} else if (obj == btnDelRegion) {
+					delRegion();
+				}
+			}
+
+		};
+		btnAddRegion.addSelectionListener(listener);
+		btnDelRegion.addSelectionListener(listener);
+	}
+	
+
+	private void delRegion() {
+		Object entity = table.getSelection();
+		if (entity != null) {
+			service.delete(entity);
+			initData();
+		}
+	}
+
+	private void addRegion() {
+//		AddRegionDialog dialog = new AddRegionDialog(getShell());
+//		if (dialog.open() == 0) {
+//			initData();
+//		}
+		table.addRow(RscObjectUtils.createRegion());
 	}
 
 	@Override
 	public void initData() {
-		List<Tb1049RegionEntity> list= (List<Tb1049RegionEntity>) beandao.getAll(Tb1049RegionEntity.class);
+		List<Tb1049RegionEntity> list= service.getAllRegionList();
 		if (list != null) {
 			table.setInput(list);
 		}
