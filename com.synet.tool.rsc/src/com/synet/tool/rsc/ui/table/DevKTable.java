f513ab98ee47099feca2cb52257ddb4dbfe9f3e8
github-minwang <minwang.gml@gmail.com>
2018-08-29 00:56:55 +0800

    Update：重写导出数据接口

--- com.synet.tool.rsc/src/com/synet/tool/rsc/ui/table/DevKTable.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/ui/table/DevKTable.java
@@ -1,9 +1,22 @@
 package com.synet.tool.rsc.ui.table;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.excelutils2007.ExcelUtils;
+
 import org.eclipse.swt.widgets.Composite;
+import com.shrcn.found.common.util.ObjectUtil;
+import com.shrcn.found.common.util.StringUtil;
+import com.shrcn.found.ui.UICommonConstants;
+import com.shrcn.found.ui.model.IField;
 import com.shrcn.found.ui.model.TableConfig;
 import com.shrcn.found.ui.table.DefaultKTable;
 import com.shrcn.found.ui.table.RKTable;
+import com.shrcn.found.ui.util.DialogHelper;
+import com.synet.tool.rsc.util.ExcelFileManager2007;
 
 
 public class DevKTable extends RKTable {
@@ -18,4 +31,53 @@ public class DevKTable extends RKTable {
 		table = new DefaultKTable(parent, tablemodel);	
 	}
 	
+	/**
+	 * 重写父类导出Excel方法，改为导出.xlsx格式
+	 */
+	@Override
+	public void exportExcel(String title) {
+		String fileName = DialogHelper.getSaveFilePath("保存", "", new String[]{"*.xlsx"});
+		if (fileName == null)
+			return;
+		Map<String, String> mapTitle = new LinkedHashMap<String, String>();
+		mapTitle.put("key0", title);
+		Map<String, String> mapSep = new LinkedHashMap<String, String>();
+		mapSep.put("key0", "");
+		Map<String, String> mapTime = new LinkedHashMap<String, String>();
+		mapTime.put("printDate", StringUtil.getCurrentTime("yyyy年MM月dd日HH点mm分ss秒"));
+		
+		IField[] vfields = getExportFields();
+		int fLen = vfields.length;
+		String[] fields = new String[fLen];
+		for (int i = 0; i < fields.length; i++) {
+			IField f = vfields[i];
+			fields[i] = f.getTitle();
+		}
+		
+		List<String[]> exportData = new ArrayList<String[]>();
+		if (getInput() != null) {
+			int index = 1;
+			for (Object o : getInput()) {
+				String[] row = new String[fLen];
+				for (int i=0; i<fLen; i++) {
+					IField f = vfields[i];
+					String fieldName = f.getName();
+					if ("index".equals(fieldName))
+						row[i] = "" + index;
+					else
+						row[i] = "" + ObjectUtil.getProperty(o, fieldName);
+				}
+				exportData.add(row);
+				index++;
+			}
+		}
+		
+		ExcelUtils.addValue("title", mapTitle);
+		ExcelUtils.addValue("sep", mapSep);
+		ExcelUtils.addValue("time", mapTime);
+		ExcelUtils.addValue("width", fLen - 1);
+		ExcelUtils.addValue("fields", fields);
+		ExcelUtils.addValue("data", exportData);
+		ExcelFileManager2007.saveExcelFile(getClass(), UICommonConstants.EXCEL_COMM_EXPORT_2007, fileName);
+	}
 }
