5c0aa5c5b093bbee4a5a207fe9062a6a7af97ab9
ChalmersA <chench80@126.com>
2018-09-13 13:55:34 +0800

    Add

---
+++ com.synet.tool.rsc/test/com/synet/tool/rsc/util/test/RuleManagerTest.java
@@ -0,0 +1,18 @@
+package com.synet.tool.rsc.util.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.synet.tool.rsc.util.F1011_NO;
+
+public class RuleManagerTest {
+
+	@Test
+	public void testGetRule() {
+		System.out.println(F1011_NO.IED_COMM.getId());
+//		Rule r = RuleManager.getInstance().getRule(1);
+//		assertNotNull(r);
+	}
+
+}
