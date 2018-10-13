2911a33e4ee89ca416932fcc97583c6f18f357db
github-minwang <minwang.gml@gmail.com>
2018-10-13 19:05:38 +0800

    update：导出光强与端口原始数据

--- com.synet.tool.rsc/src/com/synet/tool/rsc/editor/imp/ImpPortLightEditor.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/editor/imp/ImpPortLightEditor.java
@@ -5,11 +5,16 @@
  */
 package com.synet.tool.rsc.editor.imp;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -17,12 +22,16 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 
 import com.shrcn.found.ui.editor.IEditorInput;
+import com.shrcn.found.ui.model.IField;
+import com.shrcn.found.ui.util.ProgressManager;
 import com.shrcn.found.ui.util.SwtUtil;
 import com.shrcn.tool.found.das.impl.HqlDaoImpl;
 import com.synet.tool.rsc.DBConstants;
+import com.synet.tool.rsc.dialog.ExportIedDialog;
 import com.synet.tool.rsc.model.IM100FileInfoEntity;
 import com.synet.tool.rsc.model.IM106PortLightEntity;
 import com.synet.tool.rsc.model.Tb1006AnalogdataEntity;
+import com.synet.tool.rsc.model.Tb1046IedEntity;
 import com.synet.tool.rsc.model.Tb1048PortEntity;
 import com.synet.tool.rsc.model.Tb1058MmsfcdaEntity;
 import com.synet.tool.rsc.service.AnalogdataService;
@@ -30,6 +39,7 @@ import com.synet.tool.rsc.service.ImprotInfoService;
 import com.synet.tool.rsc.service.MmsfcdaService;
 import com.synet.tool.rsc.service.PortEntityService;
 import com.synet.tool.rsc.ui.TableFactory;
+import com.synet.tool.rsc.util.DateUtils;
 
 /**
  * 导入信息->光强与端口关联表 树菜单编辑器。
@@ -68,7 +78,8 @@ public class ImpPortLightEditor extends ExcelImportEditor {
 		GridData btData = new GridData();
 		btData.horizontalAlignment = SWT.RIGHT;
 		
-		Composite btComp = SwtUtil.createComposite(container, btData, 2);
+		Composite btComp = SwtUtil.createComposite(container, btData, 3);
+		btExport = SwtUtil.createPushButton(btComp, "导出Excel", new GridData());
 		btCheck = SwtUtil.createPushButton(btComp, "冲突检查", new GridData());
 		btImport = SwtUtil.createPushButton(btComp, "导入光强与端口", new GridData());
 		table =TableFactory.getPortLightTable(container);
@@ -87,6 +98,13 @@ public class ImpPortLightEditor extends ExcelImportEditor {
 			}
 		});
 		
+		btExport.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				exportExcel();
+			}
+		});
+		
 		btCheck.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -101,6 +119,64 @@ public class ImpPortLightEditor extends ExcelImportEditor {
 				importData();
 			}
 		});
+	}
+	
+	protected void exportExcel() {
+		ExportIedDialog dialog = new ExportIedDialog(getShell());
+		if (dialog.open() == IDialogConstants.OK_ID) {
+			final String filePath = dialog.getFilePath();
+			final List<Tb1046IedEntity> ieds = dialog.getIeds();
+			if (filePath == null) return;
+			ProgressManager.execute(new IRunnableWithProgress() {
+				
+				@Override
+				public void run(final IProgressMonitor monitor) throws InvocationTargetException,
+				InterruptedException {
+					IField[] vfields = getExportFields();
+					if (ieds != null && ieds.size() > 0) {
+						monitor.beginTask("开始导出", ieds.size());
+						long start = System.currentTimeMillis();
+						for (Tb1046IedEntity ied : ieds) {
+							monitor.setTaskName("正在导出装置[" + ied.getF1046Name() + "]数据");
+							List<Object> list = new ArrayList<>();
+							List<Tb1058MmsfcdaEntity> mmsList = mmsfcdaService.getMmsfcdaByIed(ied);
+							if (mmsList != null && mmsList.size() > 0) {
+								for (Tb1058MmsfcdaEntity mms : mmsList) {
+									IM106PortLightEntity entity = new IM106PortLightEntity();
+									entity.setDevName(ied.getF1046Name());
+									entity.setDevDesc(ied.getF1046Desc());
+									entity.setOpticalRefAddr(mms.getF1058RefAddr());
+									Tb1006AnalogdataEntity analogdataEntity = (Tb1006AnalogdataEntity) 
+											analogdataService.getById(Tb1006AnalogdataEntity.class, mms.getDataCode());
+									if (analogdataEntity != null) {
+										entity.setOpticalDesc(analogdataEntity.getF1006Desc());
+										if (analogdataEntity.getParentCode() != null) {
+											Tb1048PortEntity portEntity = (Tb1048PortEntity) portEntityService
+													.getById(Tb1048PortEntity.class, analogdataEntity.getParentCode());
+											if (portEntity != null) {
+												entity.setBoardCode(portEntity.getTb1047BoardByF1047Code().getF1047Slot());
+												entity.setPortCode(portEntity.getF1048No());
+											}
+										}
+									}
+									list.add(entity);
+								}
+							}
+							if (list.size() > 0) {
+								String dateStr = DateUtils.getDateStr(new Date(), DateUtils.DATE_DAY_PATTERN_);
+								String fileName = filePath + "/" + ied.getF1046Name() + "光强与端口关联表" + dateStr + ".xlsx";
+								exportTemplateExcel(fileName, "光强与端口关联表", vfields, list);
+							}
+							monitor.worked(1);
+						}
+						long time = (System.currentTimeMillis() - start) / 1000;
+						monitor.setTaskName("导出耗时：" + time + "秒");
+						monitor.done();
+						
+					}
+				}
+			}, true);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
