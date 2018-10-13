955dc53ab5a9b166eae992b2de7841cfae68c486
github-minwang <minwang.gml@gmail.com>
2018-10-13 21:57:20 +0800

    update：导出开入信号原始数据

--- com.synet.tool.rsc/src/com/synet/tool/rsc/editor/imp/ImpStatusInEditor.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/editor/imp/ImpStatusInEditor.java
@@ -5,10 +5,15 @@
  */
 package com.synet.tool.rsc.editor.imp;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -17,16 +22,21 @@ import org.eclipse.swt.widgets.Composite;
 
 import com.shrcn.found.common.util.StringUtil;
 import com.shrcn.found.ui.editor.IEditorInput;
+import com.shrcn.found.ui.model.IField;
+import com.shrcn.found.ui.util.ProgressManager;
 import com.shrcn.found.ui.util.SwtUtil;
 import com.synet.tool.rsc.DBConstants;
+import com.synet.tool.rsc.dialog.ExportIedDialog;
 import com.synet.tool.rsc.model.IM100FileInfoEntity;
 import com.synet.tool.rsc.model.IM104StatusInEntity;
+import com.synet.tool.rsc.model.Tb1046IedEntity;
 import com.synet.tool.rsc.model.Tb1058MmsfcdaEntity;
 import com.synet.tool.rsc.model.Tb1062PinEntity;
 import com.synet.tool.rsc.service.ImprotInfoService;
 import com.synet.tool.rsc.service.MmsfcdaService;
 import com.synet.tool.rsc.service.PinEntityService;
 import com.synet.tool.rsc.ui.TableFactory;
+import com.synet.tool.rsc.util.DateUtils;
 
 /**
  * 导入信息->开入信号映射表 树菜单编辑器。
@@ -61,7 +71,8 @@ public class ImpStatusInEditor extends ExcelImportEditor {
 		titleList = SwtUtil.createList(container, gridData);
 		GridData btData = new GridData();
 		btData.horizontalAlignment = SWT.RIGHT;
-		Composite btComp = SwtUtil.createComposite(container, btData, 2);
+		Composite btComp = SwtUtil.createComposite(container, btData, 3);
+		btExport = SwtUtil.createPushButton(btComp, "导出Excel", new GridData());
 		btCheck = SwtUtil.createPushButton(btComp, "冲突检查", new GridData());
 		btImport = SwtUtil.createPushButton(btComp, "导入开入信号", new GridData());
 		table =TableFactory.getStatusInTable(container);
@@ -80,6 +91,13 @@ public class ImpStatusInEditor extends ExcelImportEditor {
 			}
 		});
 		
+		btExport.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				exportExcel();
+			}
+		});
+		
 		btCheck.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
@@ -94,6 +112,54 @@ public class ImpStatusInEditor extends ExcelImportEditor {
 				importData();
 			}
 		});
+	}
+	
+	protected void exportExcel() {
+		ExportIedDialog dialog = new ExportIedDialog(getShell());
+		if (dialog.open() == IDialogConstants.OK_ID) {
+			final String filePath = dialog.getFilePath();
+			final List<Tb1046IedEntity> ieds = dialog.getIeds();
+			if (filePath == null) return;
+			ProgressManager.execute(new IRunnableWithProgress() {
+				
+				@Override
+				public void run(final IProgressMonitor monitor) throws InvocationTargetException,
+				InterruptedException {
+					IField[] vfields = getExportFields();
+					if (ieds != null && ieds.size() > 0) {
+						monitor.beginTask("开始导出", ieds.size());
+						long start = System.currentTimeMillis();
+						for (Tb1046IedEntity ied : ieds) {
+							monitor.setTaskName("正在导出装置[" + ied.getF1046Name() + "]数据");
+							List<Object> list = new ArrayList<>();
+							
+							List<Tb1062PinEntity> pinList = pinEntityService.getByIed(ied);
+							if (pinList != null && pinList.size() > 0) {
+									for (Tb1062PinEntity pinEntity : pinList) {
+									IM104StatusInEntity entity = new IM104StatusInEntity();
+									entity.setDevName(ied.getF1046Name());
+									entity.setDevDesc(ied.getF1046Desc());
+									entity.setPinRefAddr(pinEntity.getF1062RefAddr());
+									entity.setPinDesc(pinEntity.getF1062Desc());
+									list.add(entity);
+								}
+							}
+							
+							if (list.size() > 0) {
+								String dateStr = DateUtils.getDateStr(new Date(), DateUtils.DATE_DAY_PATTERN_);
+								String fileName = filePath + "/" + ied.getF1046Name() + "开入信号映射表" + dateStr + ".xlsx";
+								exportTemplateExcel(fileName, "开入信号映射表", vfields, list);
+							}
+							monitor.worked(1);
+						}
+						long time = (System.currentTimeMillis() - start) / 1000;
+						monitor.setTaskName("导出耗时：" + time + "秒");
+						monitor.done();
+						
+					}
+				}
+			}, true);
+		}
 	}
 
 	@SuppressWarnings("unchecked")
