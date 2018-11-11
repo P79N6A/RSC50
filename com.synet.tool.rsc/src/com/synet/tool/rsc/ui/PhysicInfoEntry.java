01c7195f071977d8b86abe9cfbc32db772121556
ChalmersA <chench80@126.com>
2018-11-11 12:45:58 +0800

    Update:增加区域屏柜导入功能

---
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/ui/PhysicInfoEntry.java
@@ -0,0 +1,13 @@
+package com.synet.tool.rsc.ui;
+
+import com.shrcn.found.ui.model.ConfigTreeEntry;
+import com.shrcn.found.ui.model.ITreeEntry;
+
+public class PhysicInfoEntry extends ConfigTreeEntry {
+
+	public PhysicInfoEntry(ITreeEntry parent, String name, String desc,
+			String icon, String editorId) {
+		super(parent, name, desc, icon, editorId);
+	}
+
+}
