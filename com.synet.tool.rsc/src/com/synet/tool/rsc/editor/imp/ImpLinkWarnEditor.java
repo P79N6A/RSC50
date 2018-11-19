72c0134e82f62fac21000ef3f855d2212f517a45
ChalmersA <chench80@126.com>
2018-11-19 19:30:49 +0800

    Update：支持链路告警原始数据导出

--- com.synet.tool.rsc/src/com/synet/tool/rsc/editor/imp/ImpLinkWarnEditor.java
+++ com.synet.tool.rsc/src/com/synet/tool/rsc/editor/imp/ImpLinkWarnEditor.java
@@ -5,21 +5,35 @@
  */
 package com.synet.tool.rsc.editor.imp;
 
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 
+import com.shrcn.found.common.dict.DictManager;
 import com.shrcn.found.common.util.StringUtil;
 import com.shrcn.found.ui.editor.IEditorInput;
+import com.shrcn.found.ui.model.IField;
+import com.shrcn.found.ui.util.ProgressManager;
 import com.shrcn.found.ui.util.SwtUtil;
+import com.shrcn.found.ui.view.ConsoleManager;
 import com.shrcn.found.ui.view.Problem;
+import com.shrcn.tool.found.das.impl.HqlDaoImpl;
 import com.synet.tool.rsc.DBConstants;
+import com.synet.tool.rsc.dialog.ExportIedDialog;
 import com.synet.tool.rsc.model.IM100FileInfoEntity;
 import com.synet.tool.rsc.model.IM110LinkWarnEntity;
+import com.synet.tool.rsc.model.Tb1016StatedataEntity;
+import com.synet.tool.rsc.model.Tb1046IedEntity;
 import com.synet.tool.rsc.model.Tb1058MmsfcdaEntity;
 import com.synet.tool.rsc.model.Tb1061PoutEntity;
 import com.synet.tool.rsc.model.Tb1065LogicallinkEntity;
@@ -28,6 +42,7 @@ import com.synet.tool.rsc.service.LogicallinkEntityService;
 import com.synet.tool.rsc.service.MmsfcdaService;
 import com.synet.tool.rsc.service.PoutEntityService;
 import com.synet.tool.rsc.ui.TableFactory;
+import com.synet.tool.rsc.util.DateUtils;
 
 /**
  * 导入信息->告警与链路关联表 树菜单编辑器。
@@ -75,9 +90,126 @@ public class ImpLinkWarnEditor extends ExcelImportEditor {
 	}
 	
 	protected void exportExcel() {
-		table.exportExcel2007();
+		ExportIedDialog dialog = new ExportIedDialog(getShell());
+		if (dialog.open() == IDialogConstants.OK_ID) {
+			final String filePath = dialog.getFilePath();
+			final List<Tb1046IedEntity> ieds = dialog.getIeds();
+			if (filePath == null) return;
+			ProgressManager.execute(new IRunnableWithProgress() {
+				
+				@Override
+				public void run(final IProgressMonitor monitor) throws InvocationTargetException,
+				InterruptedException {
+					IField[] vfields = getExportFields();
+					if (ieds != null && ieds.size() > 0) {
+						int totalWork = ieds.size() * 2 + 2;
+						monitor.beginTask("正在导出", totalWork);
+						long start = System.currentTimeMillis();
+						ConsoleManager console = ConsoleManager.getInstance();
+						List<Object> linkList = new ArrayList<>();
+						List<Object> mmsList = new ArrayList<>();
+						for (Tb1046IedEntity ied : ieds) {
+							if (monitor.isCanceled()) {
+								break;
+							}
+							String iedName = ied.getF1046Name();
+							monitor.setTaskName("正在导出装置[" + iedName + "]数据");
+							//导出逻辑链路部分
+							linkList.addAll(getLinkData(ied));
+							monitor.worked(1);
+							//导出告警信号部分
+							mmsList.addAll(getWarnData(ied));
+							monitor.worked(1);
+						}
+						String dateStr = DateUtils.getDateStr(new Date(), DateUtils.DATE_DAY_PATTERN_);
+						//导出逻辑链路部分
+						if (linkList.size() > 0) {
+							String fileName = filePath + "/告警与链路关联表(逻辑链路部分)" + dateStr + ".xlsx";
+							exportTemplateExcel(fileName, "逻辑链路信息表", vfields, linkList);
+						} else {
+							console.append("所选装置模型中不存在逻辑链路信息！");
+						}
+						monitor.worked(1);
+						//导出告警信号部分
+						if (mmsList.size() > 0) {
+							String fileName = filePath + "/告警与链路关联表(告警信号部分)" + dateStr + ".xlsx";
+							exportTemplateExcel(fileName, "链路告警信号表", vfields, mmsList);
+						} else {
+							console.append("所选装置模型中不存在链路告警信号！");
+						}
+						monitor.worked(1);
+						long time = (System.currentTimeMillis() - start) / 1000;
+						console.append("导出耗时：" + time + "秒");
+						monitor.done();
+					}
+				}
+			}, true);
+		}
 	}
 	
+	/**
+	 * 102,103
+	 * @param ied
+	 * @return
+	 */
+	protected List<Object> getWarnData(Tb1046IedEntity ied) {
+		List<Object> warns = new ArrayList<>();
+		HqlDaoImpl hqlDao = HqlDaoImpl.getInstance();
+		String hql = "from " + Tb1016StatedataEntity.class.getName() + 
+				" where f1011No in (102, 103) and tb1046IedByF1046Code=:ied";
+		Map<String, Object> params = new HashMap<>();
+		params.put("ied", ied);
+		int count = hqlDao.getCount(hql, params);
+		int pageSize = 500;
+		int pageTotal = count / pageSize;
+		if (count % pageSize > 0) {
+			pageTotal++;
+		}
+		DictManager dict = DictManager.getInstance();
+		for (int p = 1; p < pageTotal + 1; p++) {
+			List<?> list = hqlDao.getListByHqlAndPage(hql, params, p, pageSize);
+			for (Object o : list) {
+				Tb1016StatedataEntity st = (Tb1016StatedataEntity) o;
+				IM110LinkWarnEntity warn = new IM110LinkWarnEntity();
+				warn.setDevName(ied.getF1046Name());
+				warn.setDevDesc(ied.getF1046Desc());
+				warn.setMmsDesc(st.getF1016Desc());
+				warn.setMmsRefAddr(st.getF1016AddRef());
+				warn.setF1011No(dict.getNameById("F1011_NO", st.getF1011No()));
+				warns.add(warn);
+			}
+		}
+		return warns;
+	}
+
+	protected List<Object> getLinkData(Tb1046IedEntity ied) {
+		List<Object> links = new ArrayList<>();
+		String hql = "from " + Tb1065LogicallinkEntity.class.getName() +
+				" where tb1046IedByF1046CodeIedRecv=:ied";
+		Map<String, Object> params = new HashMap<>();
+		params.put("ied", ied);
+		HqlDaoImpl hqlDao = HqlDaoImpl.getInstance();
+		int count = hqlDao.getCount(hql, params);
+		int pageSize = 500;
+		int pageTotal = count / pageSize;
+		if (count % pageSize > 0) {
+			pageTotal++;
+		}
+		for (int p = 1; p < pageTotal + 1; p++) {
+			List<?> list = hqlDao.getListByHqlAndPage(hql, params, p, pageSize);
+			for (Object o : list) {
+				Tb1065LogicallinkEntity link = (Tb1065LogicallinkEntity) o;
+				IM110LinkWarnEntity warn = new IM110LinkWarnEntity();
+				warn.setDevName(ied.getF1046Name());
+				warn.setDevDesc(ied.getF1046Desc());
+				warn.setSendDevName(link.getTb1046IedByF1046CodeIedSend().getF1046Name());
+				warn.setCbRef(link.getBaseCbByCdCode().getCbId());
+				links.add(warn);
+			}
+		}
+		return links;
+	}
+
 	@SuppressWarnings("unchecked")
 	@Override
 	protected void doImport(IProgressMonitor monitor) {
